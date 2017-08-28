c5fa6142d11fb58d94f7dd5654dc3c46342921cc
Saurabh Pati <saurabhpati.pati@gmail.com>
2017-08-28 09:36:23 -0700

    adding handlebar files

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 // Starts up the node server by serving the host index.html which is the main page of the application.
 const express = require('express'),
-    app = express();
+    app = express(),
     port = process.env.port || 8080,
-    fs = require('fs');
+    fs = require('fs'),
+    handlebars = require('express-handlebars');
 
 // This is to serve public js files.
 app.use('/js', express.static('src/public/js'));
 app.use('/css', express.static('src/public/css'));
 app.use('/img', express.static('src/public/img'));
 app.use('/fonts', express.static('src/public/fonts'));
+app.set('views', './src/app/views');
+app.set('view engine', '.hbs');
+app.engine('.hbs', handlebars({
+    extname: '.hbs'
+}));
 
 app.get('/', (request, response) => {
-    fs.createReadStream('./index.html').pipe(response);
+    fs.createReadStream('index.html').pipe(response);
+    //response.render('index');
 });
 
 app.listen(port, () => {
