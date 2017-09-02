b4488dcba6e3ad10962b2b71d65aa50b25537973
Saurabh Pati <saurabhpati.pati@gmail.com>
2017-09-02 12:16:12 -0700

    Removing handlebars and templating.

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 // Starts up the node server for the application. 
 const express = require('express'),
     port = process.env.port || 8080,
-    serverInitializer = require('./server.init.js'),
-    app = serverInitializer.initApp(express);
+    app = express();
+
+app.use('/js', express.static('src/public/js'));
+app.use('/css', express.static('src/public/css'));
+app.use('/img', express.static('src/public/img'));
+app.use('/fonts', express.static('src/public/fonts'));
 
 app.get('/', (request, response) => {
-    response.render('index');
+    response.sendFile(__dirname + '/index.html');
 });
 
 app.listen(port, () => {
