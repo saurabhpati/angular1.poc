76caae98e4199137f64e15928e94cca14297a27f
Saurabh Pati <saurabhpati.pati@gmail.com>
2017-09-10 22:31:51 +0530

    moving code from index to templates using directives.

--- src/app/directives.js
+++ src/app/directives.js
@@ -1,9 +1,44 @@
 (() => {
-    sbApp.directive('sbNavigation', function () {
+
+    sbNavigation = () => {
         return {
             restrict: 'E',
             templateUrl: 'app/views/navigation.html'
-        }
-    });
+        };
+    }
+
+    sbHeader = () => {
+        return {
+            restrict: 'E',
+            templateUrl: 'app/views/header.html'
+        };
+    }
+
+    sbAbout = () => {
+        return {
+            restrict: 'E',
+            templateUrl: 'app/views/about.html'
+        };
+    }
+
+    sbContact = () => {
+        return {
+            restrict: 'E',
+            templateUrl: 'app/views/contact.html'
+        };
+    }
+
+    sbFooter = () => {
+        return {
+            restrict: 'E',
+            templateUrl: 'app/views/footer.html'
+        };
+    }
+
+    sbApp.directive('sbNavigation', sbNavigation);
+    sbApp.directive('sbHeader', sbHeader);
+    sbApp.directive('sbAbout', sbAbout);
+    sbApp.directive('sbContact', sbContact);
+    sbApp.directive('sbFooter', sbFooter);
 })();
 
